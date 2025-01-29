03254d10b206631428dd34275b0f6e4e0651751f
Michael Strobel <mstrobel97@gmail.com>
2025-01-29 10:14:14 +0100

    refactor: update flake.nix and remove unnecessary files

--- flake.nix
+++ flake.nix
@@ -1,22 +1,62 @@
 {
   inputs = {
-    naersk.url = "github:nix-community/naersk/master";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, utils, naersk }:
-    utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    let
+      overlay = final: prev: {
+        kibadda = (prev.kibadda or { }) // {
+          powermenu = final.pkgs.rustPlatform.buildRustPackage {
+            name = "powermenu";
+            cargoHash = "sha256-NE3PhsRPMGFKhyc7ItlJLf0z5+Dzx6G5Jkow+kD+028=";
+            src = self;
+          };
+        };
+      };
+
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+    in
+    flake-utils.lib.eachSystem supportedSystems (
+      system:
       let
-        pkgs = import nixpkgs { inherit system; };
-        naersk-lib = pkgs.callPackage naersk { };
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            overlay
+          ];
+        };
       in
       {
-        defaultPackage = naersk-lib.buildPackage ./.;
-        devShell = with pkgs; mkShell {
-          buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy ];
-          RUST_SRC_PATH = rustPlatform.rustLibSrc;
+        packages = rec {
+          default = powermenu;
+          inherit (pkgs.kibadda) powermenu;
+        };
+
+        devShells = {
+          default = pkgs.mkShell {
+            name = "powermenu-development-shell";
+            buildInputs = with pkgs; [
+              cargo
+              rustc
+              rustfmt
+              rustPackages.clippy
+            ];
+            RUST_SRC_PATH = pkgs.rustPlatform.rustLibSrc;
+          };
         };
       }
-    );
+    )
+    // {
+      overlays.default = overlay;
+    };
 }
